3eb264061685d337e6dfb011f2af23a16f7f1149
JuaFelCaiBar <JUAN.CAICEDO-B@UNIMINUTO.EDU.CO>
2024-11-22 21:05:11 -0500

    CREACIÓN EC2

--- infra/main.tf
+++ infra/main.tf
@@ -103,3 +103,42 @@ resource "aws_route_table_association" "public_association_3" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+# Crear un grupo de seguridad para permitir acceso SSH (puerto 22)
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "Allow SSH access"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acceso desde cualquier IP. Cambia esto por tu IP si es necesario
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite todo el tráfico de salida
+  }
+
+  tags = {
+    Name = "my_security_group"
+  }
+}
+
+# Crear una instancia EC2
+resource "aws_instance" "my_instance" {
+  ami           = "ami-05b1a50d6798f63cb"  # Reemplaza con el ID de la AMI (por ejemplo, Amazon Linux 2)
+  instance_type = "t3.micro"               # Tipo de instancia (puedes cambiarlo según tus necesidades)
+  subnet_id     = aws_subnet.public_subnet_1.id
+  security_group = aws_security_group.my_security_group.id
+
+  # Asociar una IP pública
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
